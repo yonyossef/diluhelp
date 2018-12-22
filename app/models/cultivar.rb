089a07e21e43b90863b8e83ac3500f4735d612cc
Yony Yossef <2345031@gmail.com>
2018-12-22 14:06:25 +0000

    added has_arms validations

--- app/models/cultivar.rb
+++ app/models/cultivar.rb
@@ -1,5 +1,6 @@
 class Cultivar < ApplicationRecord
   validates :species_id, presence: true;
+  validates :has_arms, presence: true;
   validates :name, :length => {:within => 2..25}, :allow_blank => false;
   validates :fruit_weight_gr, presence: true, numericality: { only_integer: true };
   belongs_to :species
