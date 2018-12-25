6586924e8d549788b9fcae17aa43e55dd98d46d4
Yony Yossef <2345031@gmail.com>
2018-12-25 12:57:38 +0000

    save_and_show

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,4 +1,6 @@
 class PlotsController < ApplicationController
+  before_action :set_plot, only: [:show, :edit, :update, :destroy]
+  
   def index
       #here we will present the calculator and suggest a 'new' calculation
   end
@@ -12,7 +14,9 @@ class PlotsController < ApplicationController
     
   def create
     @plot = Plot.new(plot_params)
-
+    
+    @plot.calc_results
+    
     respond_to do |format|
       if @plot.save
         format.html { redirect_to @plot, notice: 'נתוני החלקה נוספו בהצלחה למסד הנתונים' }
@@ -27,6 +31,9 @@ class PlotsController < ApplicationController
     # PATCH/PUT /cultivars/1
   # PATCH/PUT /cultivars/1.json
   def update
+      
+    @plot.calc_results
+    
     respond_to do |format|
       if @plot.update(plot_params)
         format.html { redirect_to @plot, notice: 'נתוני החלקה עודכנו בהצלחה במסד הנתונים' }
@@ -39,6 +46,10 @@ class PlotsController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_plot
+      @plot = Plot.find(params[:id])
+    end
   
   def plot_params
     params.require(:plot).permit(:name, :species_id, :cultivar_id, :yieldrec_id, :yieldwish_kg, :arms, :linedist_meters, :treedist_meters)
--- app/models/plot.rb
+++ app/models/plot.rb
@@ -3,6 +3,15 @@ class Plot < ApplicationRecord
     belongs_to :cultivars
     belongs_to :yieldrecs
     
-    validates :plot_linedist_meters, presence: true, numericality: { greater_than: 0, less_than: 20 };;
-    validates :treedist_meters, presence: true, numericality: { greater_than: 0, less_than: 20 };;
+    validates :linedist_meters, presence: true, numericality: { greater_than: 0, less_than: 20 };
+    validates :treedist_meters, presence: true, numericality: { greater_than: 0, less_than: 20 };
+    
+    def calc_results()
+        fruit_weight = Cultivar.find(self.cultivar_id).fruit_weight_gr
+        base_yield = Yieldrec.find(self.yieldrec_id).recommended_kg
+        self.trees_per_dunam = (1000 / self.linedist_meters) / self.treedist_meters
+        self.kg_per_tree = (self.yieldwish_kg + base_yield) / self.trees_per_dunam
+        self.fruits_per_tree = self.kg_per_tree * 1000 / fruit_weight
+        self.fruits_per_arm = (self.arms == 0 || self.arms.nil?) ? 0 : self.fruits_per_tree / self.arms 
+    end
 end
