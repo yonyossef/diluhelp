e819f8b68091f3ca97e27daa539839840eba0a43
Yony Yossef <2345031@gmail.com>
2018-12-29 18:13:07 +0000

    styling16_and_plot_name_validation

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -3,6 +3,7 @@ class Plot < ApplicationRecord
     belongs_to :cultivars
     belongs_to :yieldrecs
     
+    validates :name, presence: true;
     validates :linedist_meters, presence: true, numericality: { greater_than: 0, less_than: 20 };
     validates :treedist_meters, presence: true, numericality: { greater_than: 0, less_than: 20 };
     
