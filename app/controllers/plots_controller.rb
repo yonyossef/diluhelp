1a456ba3196a75ec6e42948e5b7a4983d94caf5a
Yony Yossef <2345031@gmail.com>
2018-12-30 22:01:16 +0000

    hebrew_pdf_report1

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -30,8 +30,37 @@ class PlotsController < ApplicationController
         
         bidi = Bidi.new
         
+        # t.string   "name"
+        # t.datetime "created_at",      null: false
+        
+        # t.integer  "species_id"
+        # t.integer  "cultivar_id"
+        
+        # t.integer  "linedist_meters"
+        # t.integer  "treedist_meters"
+        # t.integer  "arms"
+        # t.string   "strength"
+        # t.integer  "yieldwish_kg"
+        
+        # t.integer  "trees_per_dunam"
+        # t.integer  "kg_per_tree"
+        # t.integer  "fruits_per_tree"
+        # t.integer  "fruits_per_arm"
+        
         pdf.formatted_text_box([{ 
-          :text => (bidi.render_visual "מספר פירות לעץ: #{@plot.fruits_per_tree}")
+          :text => "שם החלקה: " + "#{@plot.name}" + "\n"},{
+          :text => "תאריך החישוב: " + bidi.render_visual("#{@plot.created_at}") + "\n"},{
+          :text => "שם המין: " + "#{Species.find(@plot.species_id).name}" + "\n"},{
+          :text => "שם הזן: " + "#{Cultivar.find(@plot.cultivar_id).name}" + "\n"},{
+          :text => "חוזק החלקה: " + bidi.render_visual("#{@plot.strength}") + "\n"},{
+          :text => "יבול רצוי: " + bidi.render_visual("#{@plot.yieldwish_kg}") + ' ק"ג' + "\n"},{
+          :text => "מרחק בין שורות: " + bidi.render_visual("#{@plot.linedist_meters}") + ' מטר' + "\n"},{
+          :text => "מרחק בין עצים: " + bidi.render_visual("#{@plot.treedist_meters}") + ' מטר' + "\n"},{
+          :text => "מספר זרועות לעץ: " + bidi.render_visual("#{@plot.arms}") + "\n"},{
+          :text => "מספר עצים לדונם: " + bidi.render_visual("#{@plot.trees_per_dunam}") + "\n"},{
+          :text => "כמות פרי לעץ: " + bidi.render_visual("#{@plot.kg_per_tree}") + ' ק"ג' + "\n"},{
+          :text => "מספר פירות לזרוע: " + bidi.render_visual("#{@plot.fruits_per_arm}") + "\n"},{
+          :text => "מספר פירות לעץ: " + bidi.render_visual("#{@plot.fruits_per_tree}") + "\n"
         }]);
         
         send_data pdf.render, 
