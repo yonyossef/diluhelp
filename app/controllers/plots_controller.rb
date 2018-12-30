f1b4f91582522857d7fb57614670daa6dd2db760
Yony Yossef <2345031@gmail.com>
2018-12-30 22:34:01 +0000

    hebrew_pdf_report2

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -46,14 +46,15 @@ class PlotsController < ApplicationController
         # t.integer  "kg_per_tree"
         # t.integer  "fruits_per_tree"
         # t.integer  "fruits_per_arm"
+        @base_yield = Yieldrec.find(@plot.yieldrec_id).recommended_kg
         
         pdf.formatted_text_box([{ 
           :text => "שם החלקה: " + "#{@plot.name}" + "\n"},{
           :text => "תאריך החישוב: " + bidi.render_visual("#{@plot.created_at}") + "\n"},{
           :text => "שם המין: " + "#{Species.find(@plot.species_id).name}" + "\n"},{
           :text => "שם הזן: " + "#{Cultivar.find(@plot.cultivar_id).name}" + "\n"},{
-          :text => "חוזק החלקה: " + bidi.render_visual("#{@plot.strength}") + "\n"},{
-          :text => "יבול רצוי: " + bidi.render_visual("#{@plot.yieldwish_kg}") + ' ק"ג' + "\n"},{
+          :text => "חוזק החלקה: " + "#{Yieldrec.find(@plot.yieldrec_id).plot_strength}" + "\n"},{
+          :text => "יבול רצוי: " + bidi.render_visual("#{@plot.yieldwish_kg + @base_yield}") + ' ק"ג' + "\n"},{
           :text => "מרחק בין שורות: " + bidi.render_visual("#{@plot.linedist_meters}") + ' מטר' + "\n"},{
           :text => "מרחק בין עצים: " + bidi.render_visual("#{@plot.treedist_meters}") + ' מטר' + "\n"},{
           :text => "מספר זרועות לעץ: " + bidi.render_visual("#{@plot.arms}") + "\n"},{
